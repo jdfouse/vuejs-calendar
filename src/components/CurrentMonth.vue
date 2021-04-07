6c4f6dc9d95781e57615bc0ce5be5f2777dd8c0a
John Fouse <john.fouse@insight.com>
2021-04-07 16:50:23 -0400

    Add inc, dec buttons, base methods

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>{{ formattedDate }}</div>
+  <div>
+    <div>{{ formattedDate }}</div>
+    <button @click="dec">-</button>
+    <button @click="inc">+</button>
+  </div>
 </template>
 <script>
 export default {
+  methods: {
+    inc() {
+      console.log('inc');
+    },
+    dec() {
+      console.log('dec');
+    },
+  },
   computed: {
     formattedDate() {
       return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format(

0787848ee543c3e73833630ac1513a4a7400af16
John Fouse <john.fouse@insight.com>
2021-04-08 13:04:54 -0400

    Add Axios action to handle add_event

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -30,7 +30,7 @@ export default {
         },
         create(){
             if (this.description.length > 0 ) {
-                this.$store.commit('addEvent', this.description);
+                this.$store.dispatch('addEvent', this.description);
                 this.description = '';
                 this.$store.commit('eventFormActive', false);
             }
